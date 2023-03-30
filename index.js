b26a8986d64f19494d00d6373162323fade6ca86
fakhruls-here <fakhrulishere@gmail.com>
2023-03-30 02:27:35 -0700

    some work to do at this momen

---
+++ index.js
@@ -0,0 +1,8 @@
+var typed = new Typed (".multiple-text",){
+    strings: ["Fronted Class", "Youtuber" , "blogger"]
+    typeSpeed: 100, 
+    BackSpeed: 100,
+    Backdelay: 1000s
+    loop: true;
+    
+}
